4b6f0b88df673a68eee475d15e25922b0d3bae63
Christian Sheen <csheen1725@gmail.com>
2021-05-01 14:45:34 -0400

    First 8KYU Checking the value of a number to the power of 2

---
+++ 8kyu/AppleTurnover.js
@@ -0,0 +1,9 @@
+/*Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.*/
+
+function apple(x) {
+    console.log(x)
+}
+
+console.log(apple('50'))
+console.log(apple(4))
+console.log(apple(50))
