5ed3e654ce95893956ed93a18c6cfe2101d8334f
Christian Sheen <csheen1725@gmail.com>
2021-05-02 10:47:33 -0400

    Stupid 8KYU boolean logic

---
+++ 8kyu/YesOrNo.js
@@ -0,0 +1,13 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a //"No" string for false.
+
+function Check(bool) {
+  return bool === true ? "Yes" : "No";
+}
+
+console.log(Check(true));
+console.log(Check(false));
+console.log(Check(false));
+console.log(Check(true));
+console.log(Check(true));
+console.log(Check(false));
+console.log(Check(true));
