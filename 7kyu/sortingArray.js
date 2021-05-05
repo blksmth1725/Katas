be8d59214b4ac8eea4c5c8cfb61ae1094d7ab7f7
Christian Sheen <csheen1725@gmail.com>
2021-05-04 23:37:00 -0400

    SumSequence.js

---
+++ 7kyu/sortingArray.js
@@ -0,0 +1,13 @@
+//You will recieve an array. Your task is to sort the array
+
+const arr1 = [5, 9, 10, 1, 8];
+const arr2 = ["orange", "apple", "duck", "pizza"];
+const arr3 = ["Porche", "Ducati", "Ferrari"];
+
+const sortTime = function (names) {
+  return names.sort();
+};
+
+console.log(sortTime(arr1));
+console.log(sortTime(arr2));
+console.log(sortTime(arr3));
