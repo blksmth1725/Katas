84e9ae1b7af636694ec9c660bea5e9f0ee9cf9a0
Christian Sheen <csheen1725@gmail.com>
2021-05-08 15:03:35 -0400

    Highest profits (highest and lowest value returned from an array)

--- 7kyu/Profits.js
+++ 7kyu/Profits.js
@@ -1,7 +1,7 @@
 //Write a function that returns both the minimum and maximum number of the given list/array.
 
 function minMax(arr) {
-  arr.sort((a, b) => a - b);
+  return [Math.min(...arr), Math.max(...arr)];
 }
 
 console.log(minMax([200, 375, 287, 1000, 70000, 356, 8320, 4, 12]));
